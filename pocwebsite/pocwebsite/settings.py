d3f3774a070fd26470c2d0927753f7c8b4d31902
Mohd Simra <mohd.simra@tothenew.com>
2022-07-22 12:25:18 +0530

    removed secret key

--- pocwebsite/pocwebsite/settings.py
+++ pocwebsite/pocwebsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from re import template
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uc*xf5r22m12h7k*3c#*4x7#c+!br38i(1x1n0m@^+g*iti4%!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
